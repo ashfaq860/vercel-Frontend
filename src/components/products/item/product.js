12ef594c8a196e20ebd597aa4dc94d74aa815342
Muhammad Ashfaq <138250358+ashfaq860@users.noreply.github.com>
2025-07-28 22:58:05 +0500

    Update product.js

--- src/components/products/item/product.js
+++ src/components/products/item/product.js
@@ -29,64 +29,7 @@ const Product = (props) => {
   }
 
   return (
-    <div className="col-12 col-sm-6 col-lg-3 text-center mt-3">
-      <div className="card scroll-animate visible">
-        <Link to={`/parts/${props._id}`}>
-          <img 
-            src={`${process.env.REACT_APP_INTERNAL_API_PATH}/${props.photo}`} 
-            className="card-img-top" 
-            alt={props.name} 
-            height="220" 
-          />
-        </Link>
-        
-        {props.latestProducts && <span className="new-product-label">New</span>}
-        {props.discount > 0 && <span className="sale-label">-11%</span>}
-        
-        <div className="card-body">
-          <div className="p-1">
-            {[...Array(5)].map((_, i) => (
-              <span 
-                key={i} 
-                className={i < (Number(props.totalStars)/Number(props.totalReviews)) ? "selected" : ""} 
-                style={{ fontSize: "18px" }}
-              >
-                &#9733;
-              </span>
-            ))}
-            <span> {props.totalReviews} {props.totalReviews > 1 ? "Reviews" : "Review"}</span>
-          </div>
-          
-          <h5 className="card-title">
-            <Link to={`/parts/${props._id}`}>
-              {props.name} <br />
-              {props.urduName}
-            </Link>
-          </h5>
-          
-          <p className="card-text">
-            {`${props.manufacturer || ''} Company's ${props.name || ''} with ${props.quality || ''} quality for ${props.category || ''}.`}
-          </p>
-          
-          <h3>
-            {props.discount > 0 && (
-              <span style={{ fontSize: "16px" }} className="text-danger">
-                <del>Rs.{props.s_price + 10}</del>
-              </span>
-            )}
-            Rs.{props.s_price}
-          </h3>
-          
-          <button 
-            className={`btn addTocart-btn ${Number(props.quantity) < 1 ? "disabled text-danger" : "enabled"}`}
-            onClick={() => getAddToCart(props)}
-            disabled={Number(props.quantity) < 1}
-          >
-            {Number(props.quantity) < 1 ? "Out Of Stock" : "Add to Basket"}
-          </button>
-        </div>
-      </div>
-    </div>
+   <></>
   )
 }
 
