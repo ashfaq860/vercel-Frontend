5b0e3b188e402513a42249cd07457b9ed24c86a4
Muhammad Ashfaq <138250358+ashfaq860@users.noreply.github.com>
2025-07-29 22:53:48 +0500

    Update loader.js

--- src/pages/loader/loader.js
+++ src/pages/loader/loader.js
@@ -4,7 +4,7 @@ const Loader = (props) => {
 
     return (<>
         <div className="loaderWrapper mt-5">
-            <h2>Loading {props.text}</h2>
+            <h2>{props.text}</h2>
             <Vortex
             visible={true}
             height="80"
